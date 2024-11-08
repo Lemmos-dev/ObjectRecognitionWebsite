b1ee3099e995e0f7874ade60a0538a750c908e31
Lemmos-dev <adamlemmey2197@gmail.com>
2024-11-08 11:17:50 +0800

    Admin Site

--- recog/admin.py
+++ recog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from recog.models import TaggedImage
+
+class TaggedImageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(TaggedImage, TaggedImageAdmin)
